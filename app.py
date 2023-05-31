dcf3494b8187ddf475dd3e1cfb9769e013978611
W3ndig0 <Jingxiang304@gmail.com>
2023-05-31 17:48:36 +0200

    website

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 from virtualTuber import VirtualTuber
 
-
 app = Flask(__name__)
 virtual_tuber = VirtualTuber()
+
+@app.route('/process_input', methods=['POST'])
+def process_input():
+    user_input = request.json['user_input']
+    response = virtual_tuber.generate_response(user_input)
+    return jsonify({'response': response})
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    user_input = request.json['user_input']
-    response = virtual_tuber.generate_response(user_input)
-    return jsonify({'response': response})
-
-
 if __name__ == '__main__':
-    print("Using device:", virtual_tuber.device)
-    app.run()
+    app.run(debug=True)
