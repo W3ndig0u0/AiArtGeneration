55d3692215ba16ef1429557afe801160725d8e93
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2023-05-31 10:26:23 +0200

    a

--- dialoGPT.py
+++ dialoGPT.py
@@ -2,34 +2,44 @@ import os
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+
 class VirtualTuber:
     def __init__(self):
         self.model_name = 'microsoft/DialoGPT-large'
         self.cache_dir = './pretrained_models'
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
         self.chat_history_ids = None
         self.secret_prompt = "Ah, my master W3ndig0. He is the one who brings me to life with his incredible skills. But be warned, he has a mysterious and dark aura surrounding him. It's best not to dig too deep into his secrets..."
         self.load_model()
 
     def load_model(self):
-        tokenizer = AutoTokenizer.from_pretrained(self.model_name, cache_dir=self.cache_dir, padding_side='left')
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, cache_dir=self.cache_dir, padding_side='left')
         try:
-            self.model = AutoModelForCausalLM.from_pretrained(self.model_name, cache_dir=self.cache_dir)
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_name, cache_dir=self.cache_dir)
             print("The model is already downloaded.")
         except OSError:
             print("ERROR: THE MODEL IS NOT DOWNLOADED.")
             print("DOWNLOADING " + self.model_name + " NOW!")
-            self.model = AutoModelForCausalLM.from_pretrained(self.model_name, cache_dir=self.cache_dir)
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_name, cache_dir=self.cache_dir)
         self.model.to(self.device)
 
     def generate_response(self, user_input):
-        tokenizer = AutoTokenizer.from_pretrained(self.model_name, cache_dir=self.cache_dir, padding_side='left')
-        user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
-        chat_history_ids = torch.cat([self.chat_history_ids, user_input_ids], dim=-1) if self.chat_history_ids is not None else user_input_ids
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name, cache_dir=self.cache_dir, padding_side='left')
+        user_input_ids = tokenizer.encode(
+            user_input + tokenizer.eos_token, return_tensors='pt')
+        chat_history_ids = torch.cat([self.chat_history_ids, user_input_ids],
+                                     dim=-1) if self.chat_history_ids is not None else user_input_ids
         with torch.no_grad():
             self.model.config.encoder_no_repeat_ngram_size = None
-            response_ids = self.model.generate(chat_history_ids.to(self.device), max_length=1000, pad_token_id=tokenizer.eos_token_id)
-        response = tokenizer.decode(response_ids[:, chat_history_ids.shape[-1]:][0], skip_special_tokens=True)
+            response_ids = self.model.generate(chat_history_ids.to(
+                self.device), max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        response = tokenizer.decode(
+            response_ids[:, chat_history_ids.shape[-1]:][0], skip_special_tokens=True)
         return response
 
     def print_available_models(self):
@@ -52,4 +62,3 @@ class VirtualTuber:
                 response = self.generate_response(user_input)
                 print("Ai:", response)
             user_input = input(">> User: ")
-
--- trainModel.py
+++ trainModel.py
@@ -2,10 +2,11 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 
 dataset = [
-    {"user_input": "Hello", "gawr_gura_response": "Hello, I'm Gawr Gura! How can I help you today?"},
-    {"user_input": "Tell me a Shark Fact", "gawr_gura_response": "Did you know that sharks have been around for millions of years?"}
+    {"user_input": "Hello",
+        "gawr_gura_response": "Hello, I'm Gawr Gura! How can I help you today?"},
+    {"user_input": "Tell me a Shark Fact",
+        "gawr_gura_response": "Did you know that sharks have been around for millions of years?"}
 ]
-
 
 model_name = 'microsoft/DialoGPT-large'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -21,10 +22,14 @@ training_args = TrainingArguments(
     logging_strategy="epoch",
 )
 
+
 def data_collator(data):
-    inputs = tokenizer(data["user_input"], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
-    inputs["labels"] = tokenizer(data["gawr_gura_response"], padding="max_length", truncation=True, max_length=512, return_tensors="pt")["input_ids"]
+    inputs = tokenizer(data["user_input"], padding="max_length",
+                       truncation=True, max_length=512, return_tensors="pt")
+    inputs["labels"] = tokenizer(data["gawr_gura_response"], padding="max_length",
+                                 truncation=True, max_length=512, return_tensors="pt")["input_ids"]
     return inputs
+
 
 trainer = Trainer(
     model=model,
